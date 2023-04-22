7b44e130b8505a3cddb2152e7f455e433354b0db
creppyfm <97473588+creppyfm@users.noreply.github.com>
2023-04-21 23:58:38 -0400

    project build files

---
+++ src/test/java/com/creppyfm/donetaskmanager/DoneTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creppyfm.donetaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoneTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
